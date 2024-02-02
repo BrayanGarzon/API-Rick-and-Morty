96a4015121d6688aa9c0e1d45386aa639eafbbed
Brayan Garzon <destroyergarzon@gmail.com>
2024-02-01 21:30:52 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import RickAndMorty from './components/RickAndMorty.vue'
+
+</script>
+
+<template>
+  
+
+  <main>
+    <RickAndMorty />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RickAndMorty.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+        <h1>Rick And Morty</h1>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
