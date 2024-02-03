69b29ad3ddb49bcf4975ac1c237954b3f52ebedb
Brayan Garzon <destroyergarzon@gmail.com>
2024-02-03 16:52:56 -0500

    agregando archivo readme

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 # API-RickAnMorty
 
-This template should help get you started developing with Vue 3 in Vite.
+This is a Vue.js component that displays a list of Rick and Morty characters, allowing you to filter them by status and search by name.
+
+## Here I explain each part:
+
+## 1. axios import:
+![Axios](./public/images/1.png)
+
+Here we import axios, a library for making HTTP requests from the browser.
+
+## 2. Definition of component data:
+![Axios](./public/images/2.png)
+
+Here you define the data properties of the component:
+
+* characters: Stores all characters obtained from the API.
+* filteredCharacters: Stores the characters that match the applied filters.
+* searchTerm: Stores the search term entered by the user.
+* activeFilter: Stores the state of the active filter.
+
+## 3. mounted method:
+![Axios](./public/images/3.png)
+
+This method is executed when the component has been mounted to the DOM. Here the fetchCharacters method is called to get the characters from the API. Once all characters have been obtained, characters are assigned to filteredCharacters to display all characters initially.
+
+## 4. fetchCharacters method:
+![Axios](./public/images/4.png)
+
+This method makes a GET request to the provided URL, which is the Rick and Morty API. Concatenates the response results to characters. If there is a next page of results, it makes a recursive request to that page. Returns a promise that resolves when all characters have been obtained.
+
+## 5. filterByStatus method:
+![Axios](./public/images/5.png)
+
+This method filters characters by status. If the status is 'all', it shows all characters. Otherwise, it shows only characters whose status matches the filter. It also updates activeFilter to reflect the active filter.
+
+## 5. filterByName method:
+![Axios](./public/images/6.png)
+
+This method filters characters by name. Shows only characters whose name includes the search term.
+
+
+## 7. Component Template: 
+The component template displays a list of characters. Each character is shown on a card with their image, name, status, species, gender, origin and location. There is also a search bar and buttons to filter by status. When a filter button is clicked, the filterByStatus method is called with the corresponding status. When text is entered into the search bar, the filterByName method is called.
+
+
 
 ## Recommended IDE Setup
 
--- src/components/RickAndMorty.vue
+++ src/components/RickAndMorty.vue
@@ -5,28 +5,54 @@ export default {
   data() {
     return {
       characters: [],
+      filteredCharacters: [],
+      searchTerm: '',
+      activeFilter: 'All',
     };
   },
   mounted() {
-    this.fetchCharacters('https://rickandmortyapi.com/api/character/');
+    this.fetchCharacters('https://rickandmortyapi.com/api/character/')
+
+    .then(() => {
+      this.filteredCharacters = this.characters;
+    });
   },
   methods: {
     fetchCharacters(url) {
-      axios.get(url)
-        .then(response => {
-          // Concatenar los personajes actuales con los nuevos
-          this.characters = this.characters.concat(response.data.results);
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        this.characters = this.characters.concat(response.data.results);
 
-          // Verificar si hay más páginas y hacer la solicitud recursiva si es necesario
-          if (response.data.info.next) {
-            this.fetchCharacters(response.data.info.next);
-            console.log(response.data.results)
-          }
-          
-        })
-        .catch(error => {
-          console.error('Error al obtener la lista de personajes:', error);
-        });
+        
+        if (response.data.info.next) {
+          this.fetchCharacters(response.data.info.next).then(resolve);
+        } else {
+          resolve();
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener la lista de personajes:', error);
+        reject(error);
+      });
+  });
+},
+
+  filterByStatus(status) {
+  const lowerCaseStatus = status.toLowerCase();
+  this.activeFilter = status;
+  if (lowerCaseStatus === 'all') {
+    this.filteredCharacters = this.characters;
+  } else {
+    this.filteredCharacters = this.characters.filter(character => character.status.toLowerCase() === lowerCaseStatus);
+  }
+},
+
+    filterByName() {
+      const searchTerm = this.searchTerm.toLowerCase();
+      this.filteredCharacters = this.characters.filter(
+        character => character.name.toLowerCase().includes(searchTerm)
+      );
     },
   },
 };
@@ -34,26 +60,49 @@ export default {
 
 <template>
     <section>
-      <h1>Lista de personajes</h1>
+      <h1>Rick and morty <br> characters</h1>
+      
       <div class="filter">
-        <div class="item">All</div>
-        <div class="item">Alive</div>
-        <div class="item">Ded</div>
-        <div class="item">Unknown</div>
+        <div class="item" :class="{ active: activeFilter === 'All' }" @click="filterByStatus('All')">All</div>
+        <div class="item" :class="{ active: activeFilter === 'Alive' }" @click="filterByStatus('Alive')">Alive</div>
+        <div class="item" :class="{ active: activeFilter === 'Dead' }" @click="filterByStatus('Dead')">Dead</div>
+        <div class="item" :class="{ active: activeFilter === 'Unknown' }" @click="filterByStatus('Unknown')">Unknown</div>
       </div>
-  
-      <div v-if="characters.length > 0" class="container-cards">
+
+      
+      <input type="text" placeholder="Search by name" v-model="searchTerm" @input="filterByName">
+      
+      <div v-if="filteredCharacters.length > 0" class="container-cards">
         <ul class="card-list">
-          <li v-for="character in characters" :key="character.id" class="card">
+          <li v-for="character in filteredCharacters" :key="character.id" class="card">
+
+            
             <div class="image">
                 <img :src="character.image" :alt="character.name">
             </div>
             
+            
             <h2>{{ character.name }}</h2>
-            <p>Specie: {{ character.species }}</p>
-            <p>Status: {{ character.status }}</p>
-            <p>Gender: {{ character.gender }}</p>
-            <!-- Otros detalles del personaje según tu necesidad -->
+            
+            
+            <div class="status">
+              <span 
+              :class="
+                character.status == 'Alive' ? 'alive' :
+                character.status == 'Dead' ? 'dead' :
+                'unknown'">
+              </span>
+              <span>{{ character.status }} - {{ character.species }}</span>
+            </div>
+
+            
+            <div class="information">
+              <span>Gender: {{ character.gender }}</span>
+              <span>Origin: {{ character.origin.name }}</span>
+              <span>Location: {{ character.location.name }}</span>
+            </div>
+            
+            
           </li>
         </ul>
       </div>
@@ -71,11 +120,11 @@ export default {
   
     section > h1 {
       text-align: center;
-      font-size: 40px;
+      font-size: 45px;
       margin-top: 4rem;
     }
     section .filter{
-        width: 20%;
+        width: 25%;
         display: flex;
         justify-content: space-around;
         align-items: center;
@@ -84,9 +133,18 @@ export default {
         font-size: 15   px;
         border-radius: 0.5rem;
         cursor: pointer;
+        margin-bottom: 1rem;
     }
     section .filter :hover{
         color: var(--text-orange);
+    }
+
+    section > input{
+      width: 25%;
+      padding: 0.9rem;
+      border-radius: 0.5rem;
+      border: none;
+      margin-bottom: 1rem;
     }
   
     section .container-cards {
@@ -102,28 +160,80 @@ export default {
       display: flex;
       flex-wrap: wrap;
       justify-content: space-around;
+      
     }
-  
+    
     .card {
-      width: 18%;
-      
+      width: 20%;
       margin: 1%;
-      border-radius: 2rem;
+      border-radius: 1rem;
       background-color: var(--background-card);
-      
       box-sizing: border-box;
       text-align: center;
       overflow: hidden;
+      transition: transform 200ms ease-in-out;
+      cursor: pointer;
     }
-  
+    .card:hover{
+      transform: scale(1.05);
+      h2{
+        color: var(--text-orange);
+      }
+    }
     .card .image {
       width: 100%;
     }
     .card .image > img {
       width: 100%;
-      
+    }
+    .card > h2{
+      margin: 5px;
+    }
+
+    .status {
+      margin: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row;
+    }
+    .status span{
+      color: var(--text-gray);
       
     }
+    .status span:first-child{
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      margin-right: 0.5rem;
+    }
+    .alive {
+        background-color: green;
+      }
+      .dead {
+        background-color: red;
+      }
+      .unknown {
+        background-color: white;
+      }
+    
+    .card .information{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+    .card .information > span{
+      color: var(--text-gray);
+      margin: 2px;
+    }
+    .card .information > span:last-child{
+      margin-bottom: 20px;
+    }
+    .item.active {
+      color: orange;
+    }
+
   </style>
   
 
